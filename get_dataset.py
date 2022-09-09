d2cd5b8143677f2532d0542803d573e466b4b866
zhj <hwangyeong@163.com>
2022-09-09 16:45:43 +0800

    .

---
+++ get_dataset.py
@@ -0,0 +1,10 @@
+
+from ogb.lsc import MAG240MDataset
+dataset = MAG240MDataset(root="dataset_path")
+
+print(dataset.num_papers) # number of paper nodes
+print(dataset.num_authors) # number of author nodes
+print(dataset.num_institutions) # number of institution nodes
+print(dataset.num_paper_features) # dimensionality of paper features
+print(dataset.num_classes) # number of subject area classes
+
